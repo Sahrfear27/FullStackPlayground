ab7e8804b9374deca099a8b27a33e70521b52b53
sahrfearmacarthy <macarthysahralves>
2025-05-01 06:39:29 -0700

    started java

---
+++ Java/OOP/inheritance.java
@@ -0,0 +1,7 @@
+package OOP;
+
+public class inheritance {
+    public static void main(String[] args) {
+        System.out.println("This is my first java program");
+    }
+}
---
+++ JavaScript/ArrayMethods/test.js
@@ -0,0 +1,18 @@
+function add() {
+  if (true) {
+    var n1 = 2;
+    var n2 = 3;
+  }
+  console.log(n1);
+  console.log(n2);
+}
+function check() {
+  if (true) {
+    let n1 = 2;
+    let n2 = 3;
+  }
+  console.log(n1);
+  console.log(n2);
+}
+
+check();
